c54b1a66d245c5ddf6ebd9a6076884feb45e9651
yann <yanndumas54230@gmail.com>
2017-11-06 15:59:16 +0100

    ajout du controller

---
+++ presentapp/control/PresentController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yann
+ * Date: 06/11/17
+ * Time: 15:56
+ */
+
+namespace presentapp\control;
+
+
+class PresentController extends \mf\control\AbstractController
+{
+
+    
+}
