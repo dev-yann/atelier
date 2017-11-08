15141c88ee109847046851bc99dd5bb36fa7f5b9
yann <yanndumas54230@gmail.com>
2017-11-08 17:40:39 +0100

    _

--- app/src/presentapp/control/PresentController.php
+++ app/src/presentapp/control/PresentController.php
@@ -277,9 +277,13 @@ class PresentController extends \mf\control\AbstractController
 
 
 
-    public function viewListeItem($msg = null){  
+    // AFFICHE LA LISTE DES ITEMS D'UNE LISTE
+    public function viewListeItem($msg = null){
+
                 $id = $this->request->get['idListe'];        
                 $l= Liste::where('idPartage','=',$id)->first();
+
+                // c'est quoi $msg
                 if($msg != ''){
                     $l['msg']=$msg;
                 }
